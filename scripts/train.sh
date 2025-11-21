3ebd6dbadf7d41370380a01a30ab630f63706557
npl <npl@users.noreply.github.com>
2025-11-21 22:31:15 +0700

    before update

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,5 +2,5 @@ python main.py \
 --config ./config/config.yaml \
 --save_dir ./save \
 --run_type train \
---device cuda:6
-# --resume_file /data2/npl/ViInfographicCaps/workspace/baseline_summarization/summarization_custom_vocab/Seq2SeqTransformer-Text-Summarization/save/checkpoints/model_1000.pth
+--device cuda:6 \
+--resume_file /datastore/npl/ViInfographicCaps/workspace/baseline_summarization/vit5_summarize/Seq2SeqTransformer-Text-Summarization/save/checkpoints/model_last.pth
